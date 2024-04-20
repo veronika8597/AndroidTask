470b5e3f2d796e3ce1ef6b92bf33f7c6979ae981
Pavel Grobov <pavelgrobov@gmail.com>
2024-04-20 15:00:23 +0300

    api-key

--- app/src/main/java/API_Model/AppModule.java
+++ app/src/main/java/API_Model/AppModule.java
@@ -7,7 +7,7 @@ public class AppModule {
 
     // Constants for the base URL and API key
     public static final String BASE_URL = "https://developer.marvel.com";
-    public static final String API_KEY = "ebabc11792bfc604e236790934607b16";
+    public static final String API_KEY = "6";
     // Static method to get a Retrofit instance
     public static Retrofit getRetroInstance() {
         return new Retrofit.Builder()
